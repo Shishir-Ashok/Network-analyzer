676ad328701a6a6970114cced68b49331ffe4a3d
Shishir-Ashok <shishirashok17@gmail.com>
2019-09-15 15:12:53 +0530

    Deleted yaml

--- flaskproject/script.sh
+++ flaskproject/script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-arp-scan -l -r 5 | tee output1.txt 
-sed '1,2d;N;$!P;$!D;$d' output1.txt | tee output2.txt
+arp-scan -l -r 2 | tee output1.txt 
+sed -i '' '1,2d;N;$!P;$!D;$d' output1.txt
 arp -a -n | grep -v "incomplete" | tee output.txt
 python parse.py 
