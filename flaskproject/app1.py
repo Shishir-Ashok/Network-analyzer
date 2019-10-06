23b22c1590c7973230cacbf7f13148f47e896955
Shishir-Ashok <shishirashok17@gmail.com>
2019-10-06 12:42:04 +0530

    login and signup

--- flaskproject/app1.py
+++ flaskproject/app1.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 import yaml
 
@@ -9,13 +9,11 @@ mysql = MySQL(app)
 
 
 db = yaml.load(open('db.yaml'))
-print(db)
+
 app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
-
-print(app)
 
 @app.route('/', methods=['GET','POST'])
 def index():
@@ -23,11 +21,17 @@ def index():
 		userDetails = request.form
 		fname = userDetails['fname']
 		lname = userDetails['lname']
-		cur = mysql.connection.cursor()
-		cur.execute("INSERT INTO USER(FNAME,LNAME) VALUES(%s,%s)",(fname,lname))
-		mysql.connection.commit()
-		cur.close()
-		# return redirect('/users')
+		fname = fname.strip()
+		lname = lname.strip()
+		print("fname :",fname,"|","Lname :",lname,"|")
+		if fname != '' and lname != '':
+			cur = mysql.connection.cursor()
+			cur.execute("INSERT INTO USER(FNAME,LNAME) VALUES(%s,%s)",(fname,lname))
+			mysql.connection.commit()
+			cur.close()
+		else:
+			return '<h1>EMPTY</h1>'
+		#return redirect('/users')
 	return render_template('index.html')
 
 @app.route('/users')
