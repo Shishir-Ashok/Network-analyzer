4f74479574de68165c2f938ba00d795fb7d21b29
Shishir-Ashok <shishirashok17@gmail.com>
2019-10-06 22:25:37 +0530

    login and sign-up pages added

--- flaskproject/app1.py
+++ flaskproject/app1.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_mysqldb import MySQL
 import yaml
+from forms import LoginForm,RegistrationForm
 
 
 app = Flask(__name__)
@@ -14,25 +15,49 @@ app.config['MYSQL_HOST'] = db['mysql_host']
 app.config['MYSQL_USER'] = db['mysql_user']
 app.config['MYSQL_PASSWORD'] = db['mysql_password']
 app.config['MYSQL_DB'] = db['mysql_db']
+app.config['SECRET_KEY'] = 'a598a7af31fd2b71f6b60c8e102bf467'
 
-@app.route('/', methods=['GET','POST'])
-def index():
-	if request.method == 'POST':
+
+
+@app.route('/sign-up', methods=['GET','POST'])
+def register():
+	# if request.method == 'POST':
+	form = RegistrationForm()
+
+	if form.validate_on_submit():
+		return render_template('layout.html')
+	return render_template('register.html',form=form)
+# @app.route('/', methods=['GET','POST'])
+# def index():
+# 	if request.method == 'POST':
+# 		userDetails = request.form
+# 		fname = userDetails['fname']
+# 		lname = userDetails['lname']
+# 		fname = fname.strip()
+# 		lname = lname.strip()
+# 		print("fname :",fname,"|","Lname :",lname,"|")
+# 		if fname != '' and lname != '':
+# 			cur = mysql.connection.cursor()
+# 			cur.execute("INSERT INTO USER(FNAME,LNAME) VALUES(%s,%s)",(fname,lname))
+# 			mysql.connection.commit()
+# 			cur.close()
+# 		else:
+# 			return '<h1>EMPTY</h1>'
+# 		#return redirect('/users')
+# 	return render_template('tindex.html')
+@app.route('/')
+@app.route('/login', methods=['GET','POST'])
+def login():
+	# if request.method == 'POST':
+	form = LoginForm()
+
+	if form.validate_on_submit():
 		userDetails = request.form
-		fname = userDetails['fname']
-		lname = userDetails['lname']
-		fname = fname.strip()
-		lname = lname.strip()
-		print("fname :",fname,"|","Lname :",lname,"|")
-		if fname != '' and lname != '':
-			cur = mysql.connection.cursor()
-			cur.execute("INSERT INTO USER(FNAME,LNAME) VALUES(%s,%s)",(fname,lname))
-			mysql.connection.commit()
-			cur.close()
-		else:
-			return '<h1>EMPTY</h1>'
-		#return redirect('/users')
-	return render_template('index.html')
+		email = userDetails['email']
+		password = userDetails['password']
+		print(email)
+		return render_template('layout.html')
+	return render_template('login.html',form=form)
 
 @app.route('/users')
 def users():
